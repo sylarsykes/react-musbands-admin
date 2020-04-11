aad4a908cdd5efb25dafa1d011b1a622abc6701d
Juan <sylar.sykes@gmail.com>
2020-04-11 10:31:44 +0200

    #3 | Commons
    resume:
    https://trello.com/c/TvOaGGcl

---
+++ src/api/function-member/collection/FunctionMemberCollection.ts
@@ -0,0 +1,5 @@
+import { BaseModelCollection } from '../../common/collection/BaseModelCollection'
+import { FunctionMemberModel } from '../model/FunctionMemberModel'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface FunctionMemberCollection extends BaseModelCollection<FunctionMemberModel> {}
--- src/api/function-member/service/find/FunctionMemberFindAllService.ts
+++ src/api/function-member/service/find/FunctionMemberFindAllService.ts
@@ -3,10 +3,11 @@ import { FunctionMemberModel } from '../../model/FunctionMemberModel'
 import { Service } from '../../../common/utils/types/Service'
 import { FetchGetRequest } from '../../../common/utils/Fetch'
 import baseFindAllService, { FindAllServiceResult } from '../../../common/service/find/BaseFindAllService'
+import { FunctionMemberCollection } from '../../collection/FunctionMemberCollection'
 
 const functionMemberFindAllService = (): Service<FindAllServiceResult<Array<FunctionMemberModel>>> => {
   const requestInfo = new FetchGetRequest('http://localhost:8085/api/admin/1.0/functionMembers', { method: 'pepito' })
-  const result = baseFindAllService<Array<FunctionMemberModel>>(requestInfo)
+  const result = baseFindAllService<FunctionMemberCollection>(requestInfo)
 
   return result
 }
